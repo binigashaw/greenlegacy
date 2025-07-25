b3ddcadba394f119690753697abe90c28bb9a411
Binyam <binigashaw@gmail.com>
2025-07-25 14:57:32 +0300

    First commit after one day document created

---
+++ greenlegacy/oneday/doctype/one_day_data/one_day_data.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Binyam Ababaw and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("One Day Data", {
+// 	refresh(frm) {
+
+// 	},
+// });
