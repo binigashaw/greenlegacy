534fbd52bdc3051432468f0fb79bdbefc6c6faec
Binyam <binigashaw@gmail.com>
2025-07-29 09:48:15 +0300

    data form and dashboard created

---
+++ greenlegacy/green_legacy/doctype/green_data/green_data.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Binyam Ababaw and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Green Data", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ greenlegacy/green_legacy/doctype/one_day_data/one_day_data.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Binyam Ababaw and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class OneDayData(Document):
+	pass
